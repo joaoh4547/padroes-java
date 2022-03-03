13c7b32ac5a009079534f15c8abdaeb1cd75b3d2
João Henrique <joaohenriquetdj@gmail.com>
2022-03-03 20:24:09 -0300

    make Application

--- ObjectPool/src/com/github/joaoh4547/objectpool/Application.java
+++ ObjectPool/src/com/github/joaoh4547/objectpool/Application.java
@@ -1,7 +1,20 @@
 package com.github.joaoh4547.objectpool;
 
+import com.github.joaoh4547.objectpool.entities.Employee;
+import com.github.joaoh4547.objectpool.pools.EmployeePool;
+import com.github.joaoh4547.objectpool.pools.Pool;
+
 public class Application {
     public static void main(String[] args) {
+        Pool<Employee> employeePool = new EmployeePool();
+        Employee employee = employeePool.acquire();
+
+
+        System.out.println(employee.getName());
+
+        employeePool.release(employee);
+        employee = employeePool.acquire();
+        System.out.println(employee.getName());
 
     }
 }
