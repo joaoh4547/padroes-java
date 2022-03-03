6192768023637aaf376f0018cc338ae52b771038
João Henrique <joaohenriquetdj@gmail.com>
2022-03-03 20:06:49 -0300

    add Employee

---
+++ ObjectPool/src/com/github/joaoh4547/objectpool/entities/Employee.java
@@ -0,0 +1,18 @@
+package com.github.joaoh4547.objectpool.entities;
+
+public class Employee {
+    private String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
