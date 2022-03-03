1825216d208f2df8c3c26fd7ebda16568a25cd79
João Henrique <joaohenriquetdj@gmail.com>
2022-03-03 12:32:58 -0300

    add ProductListExporter and Product

--- SimpleFactory/src/com/github/joaoh4547/simple_factory/entities/Product.java
+++ SimpleFactory/src/com/github/joaoh4547/simple_factory/entities/Product.java
@@ -1,4 +1,40 @@
 package com.github.joaoh4547.simple_factory.entities;
 
 public class Product {
+    private Long id;
+    private String description;
+    private Double value;
+
+    public Product() {
+    }
+
+    public Product(Long id, String description, Double value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
 }
--- SimpleFactory/src/com/github/joaoh4547/simple_factory/exporters/ProductListExporter.java
+++ SimpleFactory/src/com/github/joaoh4547/simple_factory/exporters/ProductListExporter.java
@@ -1,4 +1,43 @@
 package com.github.joaoh4547.simple_factory.exporters;
 
-public abstract class ProductListExporter implements IProductListExporter{
+import com.github.joaoh4547.simple_factory.entities.Product;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public abstract class ProductListExporter implements IProductListExporter {
+
+    protected static final Collection<String> COLUMNS_TITLES = Arrays.asList("Id", "Descrição", "Preço");
+
+    @Override
+    public final String export(Collection<Product> products) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(openTable());
+        generateLines(COLUMNS_TITLES, builder);
+        generateProductsLines(products, builder);
+        builder.append(closeTable());
+        return builder.toString();
+    }
+
+    private void generateProductsLines(Collection<Product> products, StringBuilder builder) {
+        for (Product product : products) {
+
+            Collection<String> values = Arrays.asList(
+                    product.getId().toString(),
+                    product.getDescription(),
+                    product.getValue().toString()
+            );
+            generateLines(values, builder);
+        }
+    }
+
+    private void generateLines(Collection<String> values, StringBuilder builder) {
+        builder.append(openLine());
+        for (String title : values) {
+            builder.append(openColum(title));
+            builder.append(closeColumn());
+        }
+        builder.append(closeLine());
+    }
 }
